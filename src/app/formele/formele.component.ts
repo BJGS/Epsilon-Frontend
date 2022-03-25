34e727eb25fe38b90295db028b922977d7183e2d
BJGS <nerupuclanraid@gmail.com>
2022-03-25 16:37:41 +0530

    4 files modified, 3 added

---
+++ src/app/formele/formele.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-formele',
+  templateUrl:'./formele.component.html',
+  styleUrls:['./formele.component.css']
+})
+export class FormeleComponent{}
